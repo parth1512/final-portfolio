c2837be59cd8712e38c50e99139e90605b070f2d
Parth <parth@Parths-MacBook-Air.local>
2025-06-21 01:40:37 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.png', '**/*.ttf'], // Add support for .glb, .png, and .ttf files
+  assetsInclude: ['**/*.glb', '**/*.png', '**/*.ttf','**/*.otf'], // Add support for .glb, .png, and .ttf files
   build: {
     rollupOptions: {
       external: [], // Clear external dependencies unless necessary

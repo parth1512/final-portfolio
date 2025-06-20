11b1ada048006a028af1f70ca9a71fa505193f3f
Parth <parth@Parths-MacBook-Air.local>
2025-06-21 01:29:06 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.png', '**/*.ttf'], // Add support for .glb, .png, and .ttf files
   resolve: {
     alias: {
-      '@react-three/fiber': require.resolve('@react-three/fiber'), // Resolve @react-three/fiber
+      '@react-three/fiber': path.resolve('node_modules/@react-three/fiber'),
     },
   },
   build: {

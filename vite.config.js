3e40cc4bce64672f3c4afaeb83749f1c27c83961
Parth <parth@Parths-MacBook-Air.local>
2025-06-21 01:23:51 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.glb', '**/*.png', '**/*.ttf'], // Add support for .glb files
   build: {
     rollupOptions: {
-      external: ['@react-three/fiber'], // Ensure it's marked external
+      external: ['@react-three/fiber'], // Explicitly externalize @react-three/fiber
     },
   },
 });

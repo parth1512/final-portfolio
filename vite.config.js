2eea74137b3418143a442e98201e8e83c24118eb
Parth <parth@Parths-MacBook-Air.local>
2025-06-21 01:10:54 +0530

    Fix react-three/fiber and headers

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.png', '**/*.ttf'], // Add support for .glb, .png, and .ttf files
   build: {
     rollupOptions: {
-      external: ['@react-three/fiber'], // Exclude @react-three/fiber from the bundle
-      output: {
-        manualChunks: {
-          internalDeps: ['@react-three/drei', '@react-three/rapier'], // Group internal dependencies into a chunk
-        },
-      },
+      external: ['@react-three/fiber'], // Ensure it's marked external
     },
   },
 });
